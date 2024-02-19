8afdd4b1c55a6fc7b3ed1af9691dc2080ba8262c
BlakeXYZ <blakekostroun@yahoo.com>
2024-02-18 20:52:35 -0600

    write to own db

--- Text_Detection/main.py
+++ Text_Detection/main.py
@@ -1,36 +1,162 @@
 from pathlib import Path
+import time
+import json
 
 import cv2
 import easyocr
 import matplotlib.pyplot as plt 
 
-# read img
-img_name = "img_03.jpg"
-img_path = str(Path.cwd() / 'images' / img_name)
+## Automation Steps
+#       
+#   Load Database
+#   Read Card Text
+#   Store Card Text + Confidence
+#   Get Matching Field in DB with Card Text
+#   Store Matching Field Data in DICT
+#   Write to own DB
+#
 
-my_img = cv2.imread(img_path)
+class ValidationError(Exception):
+    pass
 
-# instance text detector
-reader = easyocr.Reader(['en'], gpu=False)
+####################################################################
+'''
+Load and Read JSON Database
+'''
 
-# detect text on img
-my_text = reader.readtext(my_img)
+# read DB
+db_path = str(Path.cwd() / 'db' / 'scryfall-bulk-data.json')
+print(db_path)
 
-threshold = 0.25
+try:
+    with open(db_path, "r", encoding='utf-8') as json_file:
+        db_data = json.load(json_file)
+    print(f"Success with reading file")
 
-# draw bbox and text
-for t in my_text:
-    print(t)
+except (FileNotFoundError, json.decoder.JSONDecodeError):
+    raise ValidationError(f'Unable to load the JSON file.')
 
-    bbox, text, score =  t
 
-    if score > threshold:
-        cv2.rectangle(my_img, bbox[0], bbox[2], (0, 255, 0), 10)
-        cv2.putText(my_img, text, bbox[0], cv2.FONT_HERSHEY_COMPLEX, 2, (255, 255, 255), 15)
-        cv2.putText(my_img, text, bbox[0], cv2.FONT_HERSHEY_COMPLEX, 2, (255, 0, 0), 3)
+# read scanned DB
+scanned_card_db_path = str(Path.cwd() / 'db' / 'scanned_card_data.json')
+print(scanned_card_db_path)
 
-plt.imshow(cv2.cvtColor(my_img, cv2.COLOR_BGR2RGB))
-plt.show()
+try:
+
+    with open(scanned_card_db_path, "r") as scanned_card_json_file:
+        scanned_card_data = json.load(scanned_card_json_file)
+    print(f"Success with reading file")
+
+except (FileNotFoundError, json.decoder.JSONDecodeError):
+    raise ValidationError(f'Unable to load the JSON file.')
+
+
+
+
+
+# ####################################################################
+'''
+Read Card Text
+'''
+
+'''
+Store Card Text + Confidence
+'''
+
+
+# # Record the start time
+# start_time = time.time()
+
+# # read img
+# img_name = "img_03.jpg"
+# img_path = str(Path.cwd() / 'images' / img_name)
+
+# # Read only a section of the image (you can adjust these values)
+# x, y, w, h = 0, 0, 5000, 1000
+# my_img = cv2.imread(img_path)[y:y+h, x:x+w]
+
+# # instance text detector
+# reader = easyocr.Reader(['en'], gpu=False)
+
+# # detect text on img
+# my_text = reader.readtext(my_img)
+
+# threshold = 0.25
+
+# # draw bbox and text
+# for t in my_text:
+#     print(t)
+
+#     bbox, text, score =  t
+
+# #     if score > threshold:
+# #         cv2.rectangle(my_img, bbox[0], bbox[2], (0, 255, 0), 10)
+# #         cv2.putText(my_img, text, bbox[0], cv2.FONT_HERSHEY_COMPLEX, 2, (255, 255, 255), 15)
+# #         cv2.putText(my_img, text, bbox[0], cv2.FONT_HERSHEY_COMPLEX, 2, (255, 0, 0), 3)
+
+# # plt.imshow(cv2.cvtColor(my_img, cv2.COLOR_BGR2RGB))
+
+# # Record the end time
+# end_time = time.time()
+# elapsed_time = end_time - start_time
+# print(f"Time elapsed: {elapsed_time} seconds")
+
+# # plt.show()
+
+
+
+# ####################################################################
+    
+'''
+Get Matching Field in DB with Card Text
+'''
+
+# Specify the variable name you are looking for
+desired_variable_name = "Legolas's Quick Reflexes"  # Replace with the variable name you are looking for
+
+# Find the card with the specified variable name
+matching_card = None
+for card_entry in db_data:
+    card_name = card_entry.get("name", "").strip()
+    if card_name == desired_variable_name:
+        matching_card = card_entry
+        break
+
+
+'''
+Store Matching Field Data in DICT
+'''
+
+DICT_card_info = {}
+
+# Display the results
+if matching_card:
+
+    DICT_card_info[matching_card.get('name')] = {
+        'USD Price': matching_card.get('prices', {}).get('usd'),
+        'Rarity': matching_card.get("rarity"),
+        'URL Link': matching_card.get("scryfall_uri"),
+    }
+    
+else:
+    print(f"No card found with the name: {desired_variable_name}")
+
+print(DICT_card_info)
+
+
+
+'''
+Write to own DB
+'''
+
+# Update scanned_card_db_path in the JSON file
+
+scanned_card_data.update(DICT_card_info)
+
+with open(scanned_card_db_path, "w") as json_file:
+        json.dump(scanned_card_data, json_file, indent=4)  # The indent parameter adds pretty-printing for better readability
+
+
 
 
 
